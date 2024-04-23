22f1de9a1d78909d660b5d77d9926cd67b321af2
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-04-23 16:47:41 +0800

    Add files via upload

---
+++ src/std.h
@@ -0,0 +1,14 @@
+#ifndef STD_H_w
+#define STD_H_w
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#endif
+
